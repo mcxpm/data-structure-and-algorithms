d8cdcb16959269a10a1c0a23e593c18ef925583d
Markus Lim <93859725+mcxpm@users.noreply.github.com>
2023-06-08 23:51:45 +0800

    Add files via upload

---
+++ Percolation test/src/Percolation.java
@@ -0,0 +1,36 @@
+public class Percolation {
+    private static final int TOP = 0;
+    private final boolean[][] opened;
+    private final int size;
+    private final int bottom;
+    private int openSites;
+    private final WeightedQuickFindUF quickFindUF;
+
+    public Percolation(int n) {
+
+    }
+
+    public void open(int row, int col) {
+
+    }
+
+    public boolean isOpen(int row, int col) {
+
+    }
+
+    public boolean isFull(int row, int col) {
+
+    }
+
+    public int numberofOpenSites() {
+
+    }
+
+    public boolean percolates() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
